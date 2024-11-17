0b02a98c5cc58b17e615f0723d914f1de232c52f
Fred Trotter <fred.trotter@gmail.com>
2024-11-17 03:59:32 +0000

    before switch to bootstrap

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -66,13 +66,13 @@ function handleImageError() {
 	<h1> Award Points To ---- </h1>
 	<h3> Family and Friends Beta </h3>
 <p>An Application to use ChatGPT as a front-end for a "award point to this house/team" interface.</p>
-<p>Obvious Homage (while being different enough to be clearly Fair Use) to the Xarry Xotter franshice!!
+<p>Obvious Homage (while being different enough to be clearly Fair Use) to the Xarry Xotter franchise!!
 This is not that though!</p>
 <p>Important differences:</p>
 <ul>
 <li>Primary mode supports working together to get the total as high as possible. Houses/Teams are not strictly in competition.</li>
 <li>Track points based on person, across houses/teams, as opposed to having persons associated with one and only one team.</li>
-<li>Teams/Houses are designed get points when an indivdual does something that supports the house ideals, or lose points for violating the house ideals.</li>
+<li>Teams/Houses are designed get points when an individual does something that supports the house ideals, or lose points for violating the house ideals.</li>
 <li>Obviously you can setup your houses to map to Xogwarts.. but you can also do Xedi vs Xith or model out a point system for whatever your favorite mythical world</li>
 </ul>
 
